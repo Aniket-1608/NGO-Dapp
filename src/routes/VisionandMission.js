e5c39eb2ec984e4cb6e57d2bb1e059724bd83a13
kirithiv2000 <kirithiv@navgurukul.org>
2023-10-30 22:26:56 +0530

    material ui added

--- src/routes/VisionandMission.js
+++ src/routes/VisionandMission.js
@@ -1,5 +1,40 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: '100%' }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="540"
+          image="//www.educategirls.ngo/wp-content/themes/egindia/static/images/mobile-hero-3.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Who We Are
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Educate Girls works with the government, community and village-based volunteers called Team Balika to ensure every girl in the remotest parts of India is enrolled in school and learns well.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+
 const VisionandMission = () => {
-    return <h2>Vision & Mission</h2>;
+    return <div >
+      {/* <h2>Vision & Mission</h2>; */}
+      <ActionAreaCard/>
+
+    </div>
   };
   
   export default VisionandMission;
