a2f1a612a671b3e3619c15cb0f1c2403265353b9
kirithiv2000 <kirithiv@navgurukul.org>
2023-10-31 10:52:08 +0530

    about page layout

--- src/routes/About.js
+++ src/routes/About.js
@@ -59,24 +59,19 @@ const About = () => {
         <Route path="overview" element={<Overview />} />
       </Routes> */}
       {/* <img src={require("../components/asset/visionimg.webp")} width={'90%'} height={250} /> */}
+     <div id='overview'>  
+     <CardWithImage 
+      head="Overview" 
+      
+      
+      
+      body={"          We as a team at Rajiv Gandhi Institute of Technology are studying in our final year. Our team focused its interventions on issues concerning youth and children which brought about significant changes in their lives in terms of education, health, skill, career and sustainable livelihood. Our team also works on Environment conservation, women empowerment, Food safety and security, . Being an organisation who cares for society, our team gets engaged in projects as the situation demands under natural or manmade disasters, like COVID-19 Pandemic, Flood, Drought Relief etc social media"} 
+      img={require("../components/asset/overview.png")} />
 
-      <Card sx={{width:"90%",margin:5}}>
-      <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Overview
-          </Typography>
-          <Divider
-              variant="string"
-              sx={{ mt: "5px", pt: "5px", width: "90px", borderRadius: "5px" }}
-              color="#ff0000"
-            />
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-          We as a team at Rajiv Gandhi Institute of Technology are studying in our final year. Our team focused its interventions on issues concerning youth and children which brought about significant changes in their lives in terms of education, health, skill, career and sustainable livelihood. Our team also works on Environment conservation, women empowerment, Food safety and security, . Being an organisation who cares for society, our team gets engaged in projects as the situation demands under natural or manmade disasters, like COVID-19 Pandemic, Flood, Drought Relief etc social media
-          </Typography>
-        </CardContent>
-      </Card>
+     </div>
       <div id='vision-and-mission'>
-
+      
+    
       <CardWithImage 
       head="Vision and Mission" 
       
